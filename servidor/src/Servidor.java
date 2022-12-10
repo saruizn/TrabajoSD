277e6c0fd3b922e0ae57c93508452bd3031baba7
samu2 <samu222rn@gmail.com>
2022-12-10 17:21:32 +0100

    acabada inicialización de carpetas y archivos (dividido en métodos)

--- servidor/src/Servidor.java
+++ servidor/src/Servidor.java
@@ -8,8 +8,45 @@ import java.util.Scanner;
 
 public class Servidor {
     public static void main(String[] args){
-        System.out.println("Especifique la ruta del directorio \"recursos\" (Deje vacío para usar el predefinido en la carpeta del proyecto.)");
+        ListaUsuarios listaUsuarios=iniciarUsuarios();
+        iniciarRecursos(listaUsuarios);
+    }
+
+    public static ListaUsuarios iniciarUsuarios(){
         Scanner sc=new Scanner(System.in);
+        System.out.println("Especifique la ruta del archivo \"usuarios.xml\" (Deje vacío para usar el predefinido en la carpeta del proyecto.)");
+        String usuariosPath=sc.nextLine();
+        File usuarios=null;
+        if(usuariosPath.equals("")){
+            usuarios = new File(System.getProperty("user.dir")+"\\usuarios.xml");
+        }else{
+            usuarios = new File(usuariosPath);
+        }
+        if (!usuarios.exists()){
+            try {
+                usuarios.createNewFile();
+                FileOutputStream fos=new FileOutputStream(usuarios);
+                fos.write("<raiz/>".getBytes());
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+        ListaUsuarios listaUsuarios=null;
+        try {
+            listaUsuarios=new ListaUsuarios(usuarios);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        listaUsuarios.anadirUsuario(new Usuario("samu","samu222rn@gmail.com","1234"));
+        return listaUsuarios;
+    }
+
+    public static void iniciarRecursos(ListaUsuarios listaUsuarios){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Especifique la ruta del directorio \"recursos\" (Deje vacío para usar el predefinido en la carpeta del proyecto.)");
         String recursosPath=sc.nextLine();
         File recursos=null;
         if(recursosPath.equals("")){
@@ -23,41 +60,6 @@ public class Servidor {
                 recursos.mkdirs();
             }
         }
-        System.out.println("Especifique la ruta del archivo \"usuarios.xml\" (Deje vacío para usar el predefinido en la carpeta del proyecto.)");
-        String usuariosPath=sc.nextLine();
-        File usuarios=null;
-        if(usuariosPath.equals("")){
-            usuarios = new File(System.getProperty("user.dir")+"\\usuarios.xml");
-            if (!usuarios.exists()){
-                try {
-                    usuarios.createNewFile();
-                    FileOutputStream fos=new FileOutputStream(usuarios);
-                    fos.write("<raiz/>".getBytes());
-                    fos.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    System.exit(1);
-                }
-            }
-        }else{
-            recursos = new File(recursosPath);
-            if (!recursos.exists()){
-                try {
-                    recursos.createNewFile();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    System.exit(1);
-                }
-            }
-        }
-        ListaUsuarios listaUsuarios=null;
-        try {
-            listaUsuarios=new ListaUsuarios(usuarios);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-        listaUsuarios.anadirUsuario(new Usuario("samuel","samu@gmail.com","xddd"));
         for(Usuario u: listaUsuarios.getUsuarios()){
             File carpetaPersonal = new File(recursos.getAbsolutePath()+"\\"+u.getNombre());
             if(!carpetaPersonal.exists()){
